2eb4ce11bea0c712391646dc0bf81fd7e958ab20
Rafał Zborowski <rafal.zborowski.dev@gmail.com>
2019-12-04 21:29:54 +0100

    Made FileWithIncome constructor

---
+++ FileWithIncome.h
@@ -0,0 +1,18 @@
+#ifndef FILEWITHINCOME_H
+#define FILEWITHINCOME_H
+
+#include "income.h"
+#include "TextFile.h"
+
+using namespace std;
+
+class FileWithIncome: public TextFile {
+
+
+public:
+    FileWithIncome(string incomeFileName):TextFile( incomeFileName ){};
+
+
+};
+
+#endif
